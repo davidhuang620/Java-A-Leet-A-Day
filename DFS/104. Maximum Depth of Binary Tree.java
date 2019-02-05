ae9a65b867c18a5a9dce040d77b4b927f6722c9b
David Huang <d12yes@gmail.com>
2019-02-05 09:57:38 +0800

    Update 104. Maximum Depth of Binary Tree.java

--- DFS/104. Maximum Depth of Binary Tree.java
+++ DFS/104. Maximum Depth of Binary Tree.java
@@ -6,27 +6,62 @@
 
 // Recursion
 
+/* Keys
+1. variable counter could just be passed in the return statement.
+2. Recursion can happens in just one function.
+*/
+
 class Solution {
   public int maxDepth(TreeNode root){
-    if(node == null){return 0;}    
+    if(root == null){return 0;}
     return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
   }
 }
 
-// Iteration
+
+// Iteration DFS
+
+/* Keys
+1. edge cases: when input root node is empty
+2. Stack <type> variable = new Stack <>();
+3. Stack <Integer>
+*/
+
 class Solution {
   public int maxDepth(TreeNode root){
    
-  
-  
+    if(root == null){
+        return 0;        
+    }
+      
+    Stack<TreeNode> nodeStack = new Stack<>();
+    Stack<Integer> depthStack = new Stack<>();
+    int maxDepth = 0;
+
+    nodeStack.push(root);
+    depthStack.push(1);
+    
+    while(!nodeStack.isEmpty()){
+        TreeNode node = nodeStack.pop();
+        int depth = depthStack.pop();
+      
+        maxDepth = Math.max(maxDepth,depth);
+        
+        if(node.right != null){
+          nodeStack.push(node.right);
+          depthStack.push(depth+1);
+        }
+        
+        if(node.left != null){
+          nodeStack.push(node.left);
+          depthStack.push(depth+1);
+        }
+    }
+      
+    return maxDepth;
   }
 }
 
-
-/* Improvement
-1. variable counter could just be passed in the return statement.
-2. Recursion can happens in just one function.
-*/
 
 // Original solution
 
