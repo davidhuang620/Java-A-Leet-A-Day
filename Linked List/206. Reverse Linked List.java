fcd2f3cef1652f82f10756e318377bde4d992c9f
David Huang <d12yes@gmail.com>
2019-02-23 19:43:35 -0600

    Update 206. Reverse Linked List.java

--- Linked List/206. Reverse Linked List.java
+++ Linked List/206. Reverse Linked List.java
@@ -4,7 +4,67 @@
 
 Draw graphs so we know the relationship of the 3 nodes we need to keep track of.
 
+Recursive: 
+
+1. recursive goes two ways 
+
+    ---> forward 
+    ... code ...
+    ---> forward
+    
+    Recursion Function Call
+    (Paused and goes into the next recursion)
+    (Come back to this line After the next recursion and all of its inner recursions have finished)
+    (if we {return recursion-function-call}, or return anything, the following code will not happen)
+    
+    <--- backward
+    ... code ...
+    <--- backward
+    
+    
+2. Always traversing 2 times
+
+3. Recursion is a series of calls on call stack
+
+           4 (base case reached, return) 
+        3     3
+    2             2
+1                     1
+
+
+4. Singly LinkedList could use this technique to traverse backward
+
+5. Use a temp varialbe to store the return objects from the recursion call, so we can execute something backward.
+
+---> forward code
+
+SomeObject tempVariable = recursion-function-call ; 
+
+<--- backward code
+
+return tempVariable;
+
+######################
+
+---> forward code 
+
+return recursion-function-call;
+
+<--- any code here won't be executed
+
+
+
 */
+
+// Recursive:
+
+public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null) return head;
+    ListNode p = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return p;
+}
 
 // Iterative:
 
@@ -29,12 +89,4 @@ class Solution {
     }
 }
 
-// Recursive:
 
-public ListNode reverseList(ListNode head) {
-    if (head == null || head.next == null) return head;
-    ListNode p = reverseList(head.next);
-    head.next.next = head;
-    head.next = null;
-    return p;
-}
