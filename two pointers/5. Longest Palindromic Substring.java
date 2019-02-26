db4187daf76cb3647f56b38e65b9841f66726498
David Huang <d12yes@gmail.com>
2019-02-26 10:22:02 -0600

    Update 5. Longest Palindromic Substring.java

--- two pointers/5. Longest Palindromic Substring.java
+++ two pointers/5. Longest Palindromic Substring.java
@@ -1,22 +1,29 @@
 
 
+/*
 
+Use helper function to keep code clean.
+
+Draw to make different cases clear.
+
+while can combine with if. 
+
+*/
 
 
 class Solution {
     public String longestPalindrome(String s) {
         
-        if(s.isEmpty()){return "";}
+        if(s.isEmpty() || s == null){return "";}
         if(s.length()==1){return s;}
         
         String ans = s.substring(0,1);
         
         for (int i = 0; i < s.length(); i++){
+            
             String subString = subStringLoop( i-1, i+1, s);
             ans = ans.length() >= subString.length() ? ans : subString;
-        }
-        
-        for (int j = 0; j < s.length()-1; j++){
+
             String subString = subStringLoop( j, j+1, s);
             ans = ans.length() >= subString.length() ? ans : subString;
         }
@@ -27,16 +34,12 @@ class Solution {
     
     public String subStringLoop(int front, int back, String s){
         
-        while( front >= 0 && back < s.length()){
-            if( s.charAt(front) != s.charAt(back) ){
-                break;
-            }
+        while( front >= 0 && back < s.length() && s.charAt(front) != s.charAt(back)){
             front --;
             back ++;
         }
         
         return s.charAt(front+1) == s.charAt(back-1) ? s.substring( front+1, back) : "";
-        
     }
     
 }
