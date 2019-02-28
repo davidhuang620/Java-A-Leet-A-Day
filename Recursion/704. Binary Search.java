e74ea87afd50ab1a6fb9f0af2656e06272001770
David Huang <d12yes@gmail.com>
2019-02-28 16:27:08 -0600

    Update 704. Binary Search.java

--- Recursion/704. Binary Search.java
+++ Recursion/704. Binary Search.java
@@ -7,35 +7,6 @@ Contrast to JavaScript where inner scope do have access to outer scope, and it i
 
 */
 
-// Recursion on the original function
-
-class Solution {
-    public int search(int[] nums, int target) {
-        
-        if(nums.length == 0) { return -1; }
-        
-        int midIndex = nums.length/2;
-        int mid = nums[midIndex];
-        
-        if(mid == target){
-            return midIndex;
-        }
-        
-        if(nums.length == 1) {
-            return -1;
-        }
-        
-        if(mid > target){
-            int [] arr = Arrays.copyOfRange(nums, 0, midIndex);
-            return search(arr, target);
-        }else{
-            int [] arr = Arrays.copyOfRange(nums, midIndex+1, nums.length);
-            return midIndex + search(arr, target);
-        }
-    }
-     
-}
-
 
 // Recursion on a separated function.
 class Solution {
