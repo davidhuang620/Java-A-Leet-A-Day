1ab44e7c7c402d8b60d66ddcb5a8b7cd476edf60
David Huang <d12yes@gmail.com>
2019-02-28 16:14:06 -0600

    Update 704. Binary Search.java

--- Recursion/704. Binary Search.java
+++ Recursion/704. Binary Search.java
@@ -3,6 +3,40 @@
 */
 
 
+// Recursion on a separated function.
+
+class Solution {
+    
+    public int search(int[] nums, int target) {
+        
+        // do I have to pass in nums, and target ?
+        return binarySearch(nums, 0, nums.length-1, target);
+    }
+    
+    private int binarySearch(int[] nums, int start, int end, int target){
+        
+        int mid = (start + end)/2;
+        
+        if(nums[mid] == target){
+            return mid;
+        }
+        
+        if(start >= end){
+            return -1;
+        }
+        
+        if(nums[mid] > target){
+            return binarySearch(nums, start, mid-1, target);
+        }
+        if(nums[mid] < target){
+            return binarySearch(nums, mid+1, end, target);
+        }
+        
+        return -1;
+    }
+    
+    
+}
 
 
 
