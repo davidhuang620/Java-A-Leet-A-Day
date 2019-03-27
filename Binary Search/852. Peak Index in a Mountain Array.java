771f6805e2662d929ee3f87bd081289551100250
David Huang <d12yes@gmail.com>
2019-03-27 15:48:30 -0500

    Create 852. Peak Index in a Mountain Array.java

---
+++ Binary Search/852. Peak Index in a Mountain Array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        
+        // if (A == null || A.length == 0){
+        //     return -1;
+        // }
+        
+        int low = 0, up = A.length - 1;
+        while (low + 1 < up){
+            
+            int mid = low + (up - low) / 2;
+            if (A[mid - 1] < A[mid] && A[mid + 1] < A[mid]){
+                return mid;
+            } else if (A[mid - 1] > A[mid]){
+                up = mid;
+            } else{
+                low = mid;
+            }
+        }
+        
+        return A[low - 1] < A[low] && A[low + 1] < A[low] ? low : up;
+        
+    }
+}
