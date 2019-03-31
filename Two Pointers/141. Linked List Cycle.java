9d3ce51abd5541e59a55e298221a9e60c4ddc398
David Huang <d12yes@gmail.com>
2019-03-31 08:28:51 -0500

    Update 141. Linked List Cycle.java

--- Two Pointers/141. Linked List Cycle.java
+++ Two Pointers/141. Linked List Cycle.java
@@ -1,3 +1,42 @@
+/*
+2 Approaches
+
+Slow Fast Pointer Approach
+O(n + k) Time, O(1) Space
+
+HashTable Approach
+O(1) Time, O(n) Space
+
+*/
+
+
+
+// Slow Fast Pointer Approach
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+        if (head == null || head.next == null){
+            return false;
+        }
+        
+        ListNode pt = head;
+        ListNode pt2 = head.next.next;
+        
+        while (pt2 != null && pt2.next != null){
+            if (pt == pt2){
+                return true;
+            } 
+            pt = pt.next;
+            pt2 = pt2.next.next;
+        }
+        
+        return false;
+        
+    }
+}
+
+
 // HashTable Approach
 // O(1) Time; O(N) Space
 
