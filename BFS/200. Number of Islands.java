7a2bdea865557b65612e591610ff78705b89e80b
David Huang <d12yes@gmail.com>
2019-04-04 23:18:10 -0500

    Create 200. Number of Islands.java

---
+++ BFS/200. Number of Islands.java
@@ -0,0 +1,79 @@
+
+        // Trick
+        // Setting the visited node as 0
+        // so that we don't need another HashSet to keep track of visited slots
+       
+        // Trick
+        // => row * numbers of Col + Col
+        // using this formula so that each Int represent an unique slot
+        // No need to store another node object or int[]
+
+
+
+class Solution {
+    public int numIslands(char[][] grid) {
+        
+        if (grid == null || grid.length == 0 || grid[0].length == 0){
+            return 0;
+        }
+        
+        int nRow = grid.length;
+        int nCol = grid[0].length;
+        int count = 0;
+        for (int i = 0; i < nRow; i++){
+            for (int j = 0; j < nCol; j++){
+                // char '1' != int 1
+                if(grid[i][j] == '1'){
+                    dfs(i, j, grid);  
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    private void dfs(int i, int j, char[][] grid){
+        
+        int nCol = grid[0].length;
+        int nRow = grid.length;
+        // TRICK 
+        // => row * numbers of Col + Col
+        // using this formula so that each Int represent an unique slot
+        // No need to store another node object or int[]
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(i * nCol + j);
+
+        // Trick
+        // Setting the visited node as 0
+        // so that we don't need another HashSet to keep track of visited slots
+        grid[i][j] = '0';
+        
+        while (!queue.isEmpty()){
+            int num = queue.poll();
+            int col = num % nCol;
+            int row = num / nCol;
+            if (row + 1 < nRow && grid[row + 1][col] == '1'){
+                queue.add((row + 1) * nCol + col);
+                //change the '0' right here, or it will causes duplicates nodes in the queue 
+                grid[row+1][col] = '0';
+            }
+            if (row - 1 >= 0 && grid[row - 1][col] == '1'){
+                queue.add((row - 1) * nCol + col);
+                grid[row-1][col] = '0';
+            }
+            if (col + 1 < nCol && grid[row][col + 1] == '1'){
+                queue.add((row) * nCol + col + 1);
+                grid[row][col+1] = '0';
+            }
+            if (col - 1 >= 0 && grid[row][col - 1] == '1'){
+                queue.add((row) * nCol + col - 1);
+                grid[row][col-1] = '0';
+            }
+            
+            
+        }
+        
+    }
+    
+}
