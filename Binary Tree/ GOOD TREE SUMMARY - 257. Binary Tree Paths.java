2fff17e5e6bda127dadb193630c42ccbcd5e1139
David Huang <d12yes@gmail.com>
2019-04-11 09:30:10 -0500

    Update and rename 257. Binary Tree Paths.java to  GOOD TREE SUMMARY - 257. Binary Tree Paths.java

---
+++ Binary Tree/ GOOD TREE SUMMARY - 257. Binary Tree Paths.java
@@ -0,0 +1,101 @@
+
+/**
+
+分 Divide    recursion to left and right, (1 node -> 2 nodes)
+治 Conquer   Operation 
+            (Before Merge, dealing with one node of things)
+             sometimes recursion could be a void function
+             since we don't have anymore operation after merge 
+合 Merge     return and traverse the second time (2 nodes-> 1 node)
+
+*********
+
+分 Divide    recursion to left and right, (1 node -> 2 nodes)
+合 Merge     return and traverse the second time (2 nodes-> 1 node)
+治 Conquer   Operation 
+            (After Merge, dealing with two nodes of things)
+            sometime infos from two nodes is necesarry, ex: sum, min, max
+            Be careful of what kind of ds and return value 
+            to store more than one info
+*********     
+
+Think clearly of what parameter/ data structure we need to pass in to the next recursion. 
+
+Draw a graph if we got confused.
+
+
+ */
+
+
+
+/*
+分
+治
+合
+*/
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        
+        List<String> ans = new ArrayList<String>();
+        helper(root, "", ans);
+        return ans;
+    }
+    
+    private void helper(TreeNode root, String path, List<String>ans){
+    
+        if (root == null){
+            return;
+        }
+        path = path + root.val + "->";
+        helper(root.left, path, ans);
+        helper(root.right, path, ans);
+        // until both left and right node is null, then we reach a leaf
+        if (root.left == null && root.right == null){
+            path = path.substring(0, path.length() - 2);
+            ans.add(path);
+            return;
+        }
+    }
+}
+
+
+/*
+分 
+合
+治
+*/
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        return helper(root);
+    }
+    
+    private List<String> helper(TreeNode root){
+        List<String> res = new ArrayList<>();
+        if (root == null){
+            return res;
+        }
+        // only when both left and right is null, it is a leaf
+        // then we can return this as a path
+        if (root.left == null && root.right == null){
+            res.add("" + root.val);
+            return res;
+        }
+        // for every path in the left subtree, we need to add root.val 
+        if (root.left != null){
+            List<String> lList= helper(root.left);
+            for (String path : lList){
+                path = root.val + "->" + path;
+                res.add(path);
+            }
+        }
+        // for every path in the right subtree, we need to add root.val 
+        if (root.right != null){
+            List<String> rList= helper(root.right);
+            for (String path : rList){
+                path = root.val + "->" + path;
+                res.add(path);
+            }            
+        }
+        return res;
+    }
+}
