424dc80f72db2fbbe986290e5c429c101f2aeb28
David Huang <d12yes@gmail.com>
2019-04-15 11:36:57 -0500

    Update 90. Subsets II.java

--- Combination & DFS/90. Subsets II.java
+++ Combination & DFS/90. Subsets II.java
@@ -21,6 +21,14 @@ Which means we continue the for loop when the duplicate number appears at i > in
             // nums[i] == nums[i-1] <- it is a duplicate
             // i > index <- the duplicate will have been added ONCE into the subset when i == index,
             // every other times, we do not add in the duplicate
+            
+            凡是i= startIndex的时候，即便和前一个数相等，也是subset的第一次取到，也是最深的搜索结果一定要获得。 
+            如[1,2,2]中subset =[1,2]开始搜索, startIndex将指向第二个2开始搜索(startIndex = 2)，
+            但由于开始i==startIndex, 仍然需要取以获得[1,2,2]，只因为和前一个数相等就continue会漏掉[1,2,2]。
+            如果i > startIndex, 那么这样的subset必然已经在之前取得过了并且被搜索过， 就是重复的结果了，
+            同样对[1,2,2]计算，如subset =[1]的情况下去搜索, startIndex = 1,即从第一个2组成的[1,2]进行搜索， 
+            当完成并回溯后，下一个循环中，此时nums[2] = nums[1]且i = 2>startIndex = 1, 
+            同样的[1,2]已经在之前的回溯中都获得了全部的搜索结果，所以要continue.
 
 */
 class Solution {
