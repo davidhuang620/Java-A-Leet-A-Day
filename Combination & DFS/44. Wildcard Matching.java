4a0386594906be7b9d60ee1336f69920f58899bd
David Huang <d12yes@gmail.com>
2019-04-17 12:03:54 -0500

    Update 44. Wildcard Matching.java

--- Combination & DFS/44. Wildcard Matching.java
+++ Combination & DFS/44. Wildcard Matching.java
@@ -1,5 +1,79 @@
 /*
 
+To avoid using 2d array * 2
+we can declare the 2d array as Boolean[][] instead of boolean[][]
+Boolean is a wrapper class for boolean, which is an object
+Therefore we can check directly if Boolean[][] is null
+        if (memo[sInd][pInd] != null)
+Whereas the boolean[][] will have an compile error, cuz boolean is a primitive type, not object
+        
+
+*/
+class Solution {
+    public boolean isMatch(String s, String p) {
+     
+        if (s == null || p == null){
+            return false;
+        }
+        
+        char[] sChar = s.toCharArray();
+        char[] pChar = p.toCharArray();
+        
+        // memoization
+        Boolean[][] memo = new Boolean[s.length()][p.length()];
+
+        return dfs(sChar, pChar, 0, 0, memo);
+            
+    }
+    
+    private boolean dfs(char[] s, char[] p, int sInd, int pInd, 
+                        Boolean[][] memo){
+        
+        // case s == "" && ( p == "" or ""*....*" )
+        if (sInd == s.length){
+            for (int i = pInd; i < p.length; i++){
+                if (p[i] != '*'){
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+        if (pInd == p.length){
+            return false;
+        }
+        
+        // only if memo is Boolean[] with a capital B, Boolean
+        // 
+        if (memo[sInd][pInd] != null){
+            return memo[sInd][pInd];
+        }
+        
+        // memo
+        boolean res;
+        
+        if (s[sInd] == p[pInd] || p[pInd] == '?'){
+            res = dfs(s, p, sInd + 1, pInd + 1, memo);
+        } else if (p[pInd] == '*'){
+                    // * == s[sInd] + ...
+                    // * == s[sInd]
+                    // * == empty string
+                res = dfs(s, p, sInd + 1, pInd, memo)         
+                    || dfs(s, p, sInd + 1, pInd + 1, memo) 
+                    || dfs(s, p, sInd, pInd + 1, memo);    
+        } else{
+            res = false;
+        }
+        
+        memo[sInd][pInd] = res;        
+        return res;
+        
+    }
+    
+}
+
+/*
+
 Memoization
 2D array x 2 
 one for visited
