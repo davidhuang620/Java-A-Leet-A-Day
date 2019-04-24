061642b65f630c0bd7bff7bc10785f96b7652325
David Huang <d12yes@gmail.com>
2019-04-24 08:45:58 -0500

    Update 238. Product of Array Except Self.java

--- Dynamic Programming/238. Product of Array Except Self.java
+++ Dynamic Programming/238. Product of Array Except Self.java
@@ -3,12 +3,32 @@
 
 Observe the pattern
 Follow up: use constant space
-
-
+There are index tricks with constant space, gotta be careful
+Better to draw simple example to really understand
 */
 // constant space
 
-
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        if (nums == null || nums.length == 0){
+            return new int[0];
+        }
+        int[] ans = new int[nums.length];
+        ans[0] = 1;
+        // we add this in so that the first/last items are easier to deal with later
+        for (int i = 1; i < nums.length; i++){
+            ans[i] = ans[i-1] * nums[i - 1];
+        }
+        int product = 1;
+        for (int i = nums.length - 1; i >= 0; i--){
+            // we multiply the product after assign the ans
+            // so that the last item and first item can be add in
+            ans[i] = ans[i] * product;
+            product *= nums[i];
+        }
+        return ans;
+    }
+}
 
 
 /*
