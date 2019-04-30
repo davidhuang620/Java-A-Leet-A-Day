50231dc772a53486a9651559a848be1b92d711cb
David Huang <d12yes@gmail.com>
2019-04-30 14:34:22 -0500

    Update 200. Number of Islands.java

--- 4 - BFS/200. Number of Islands.java
+++ 4 - BFS/200. Number of Islands.java
@@ -1,4 +1,47 @@
+// short and clean version
 
+class Solution {
+    public int numIslands(char[][] grid) {
+        
+        if (grid == null || grid.length == 0 || grid[0].length == 0){
+            return 0;
+        }
+        
+        int count = 0;
+        for (int r = 0; r < grid.length; r++){
+            for (int c = 0; c < grid[0].length; c++){
+                if (grid[r][c] == '0'){
+                    continue;
+                }
+                helper(grid, r, c);
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    private void helper(char[][] grid, int r, int c){
+        
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length 
+            || grid[r][c] == '0'){           
+            return;
+        }
+        
+        grid[r][c] = '0';
+        
+        helper(grid, r + 1, c);
+        helper(grid, r - 1, c);
+        helper(grid, r, c + 1);
+        helper(grid, r, c - 1);
+        
+    }
+}
+
+
+
+
+
+// DFS
         // Trick
         // Setting the visited node as 0
         // so that we don't need another HashSet to keep track of visited slots
