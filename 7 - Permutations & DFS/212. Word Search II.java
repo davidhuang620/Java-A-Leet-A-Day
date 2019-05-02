1e66e9151ac7d716842f915e6891cf4c6d2277cc
David Huang <d12yes@gmail.com>
2019-05-01 23:09:55 -0500

    Update 212. Word Search II.java

--- 7 - Permutations & DFS/212. Word Search II.java
+++ 7 - Permutations & DFS/212. Word Search II.java
@@ -11,6 +11,80 @@
         }
 
 */
+
+// 1. Tire
+
+class Solution {
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> ans = new ArrayList<>();
+        if (board == null || board.length == 0 || board[0].length == 0
+            || words == null || words.length == 0){
+            return ans;
+        }
+        
+        TrieNode root = createTrie(words);
+        for (int r = 0; r < board.length; r++){
+            for (int c = 0; c < board[0].length; c++){
+                if (root.next[board[r][c] - 'a'] == null){
+                    continue;
+                }
+                helper(r, c, root, board, ans);
+            }
+        }
+        return ans;        
+    }
+    
+    private void helper(int r, int c, TrieNode node, char[][]board, List<String> ans){
+        
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length 
+            || board[r][c] == '#'){
+            return;
+        }
+        
+        if (node.next[board[r][c] - 'a'] == null){
+            return;
+        }
+        
+        node = node.next[board[r][c] - 'a'];
+        if (node.word != null){
+            ans.add(node.word);
+            node.word = null;   // avoid duplciate
+        }
+
+        board[r][c] = '#';
+        helper(r + 1, c, node, board, ans);
+        helper(r - 1, c, node, board, ans);
+        helper(r, c + 1, node, board, ans);
+        helper(r, c - 1, node, board, ans);
+        board[r][c] = node.val;
+        
+    }    
+    
+    class TrieNode{
+        char val;
+        String word;
+        TrieNode[] next = new TrieNode[26];
+    }
+    
+    private TrieNode createTrie(String[] words){
+        TrieNode root = new TrieNode();
+        for (String word : words){
+            TrieNode node = root;
+            for (char c : word.toCharArray()){
+                if (node.next[c - 'a'] == null){
+                    node.next[c - 'a'] = new TrieNode();    
+                }
+                node = node.next[c - 'a'];
+                node.val = c;
+            }
+            node.word = word;
+        }
+        return root;
+    }
+    
+}
+
+// 2.
 class Solution {
     public List<String> findWords(char[][] board, String[] words) {
         if (words == null || words.length == 0 || board == null
