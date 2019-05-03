4db8c541a7d9b013ea5d5be81b3c46f39899591b
David Huang <d12yes@gmail.com>
2019-05-02 19:29:42 -0500

    Update 34. Find First and Last Position of Element in Sorted Array.java

--- 2 - Binary Search/34. Find First and Last Position of Element in Sorted Array.java
+++ 2 - Binary Search/34. Find First and Last Position of Element in Sorted Array.java
@@ -2,69 +2,48 @@
         // int does not have null value
         // (int == null) will throw error
  
+
+
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-     
-        int[] arr = new int[] {-1, -1};
-        
         if (nums == null || nums.length == 0){
-            return arr;
+            return new int[]{-1, -1};
         }
         
-        findLeft(nums, target, arr);
-        findRight(nums, target, arr);
+        int[] ans = new int[2];
+        ans[0] = helper(nums, target, true);
+        ans[1] = helper(nums, target, false);
+        return ans;
         
-        return arr;
     }
     
-    private void findLeft (int[] nums, int target, int[] arr){
-
-        int left = 0, right = nums.length - 1;
-        while (left + 1 < right){
+    private int helper(int[] nums, int target, boolean first){
+        
+        int low = 0, up = nums.length - 1;
+        while(low + 1 < up){
+            int mid = low + (up - low) / 2;
             
-            int mid = left + (right - left) / 2;    
-            if (nums[mid] == target){
-                right = mid;
-            }
-            if (nums[mid] < target){
-                left = mid;
-            }            
-            if (nums[mid] > target){
-                right = mid;
+            if (nums[mid] == target && first){
+                up = mid;
+            } else if (nums[mid] == target && !first){
+                low = mid;
+            } else if (nums[mid] > target){
+                up = mid;
+            } else {
+                low = mid;
             }
         }
         
-        if (nums[right] == target){
-            arr[0] = right;
-        }
-        if (nums[left] == target){
-            arr[0] = left;
-        }
-            
-     }
+        if (first){
+            if (nums[low] == target) return low;
+            if (nums[up] == target) return up;
+            return -1;
+        } 
         
-    private void findRight (int[] nums, int target, int[] arr){
-
-        int left = 0, right = nums.length - 1;
-        while (left + 1 < right){
+        if (nums[up] == target) return up;
+        if (nums[low] == target) return low;
+        return -1;
             
-            int mid = left + (right - left) / 2;    
-            if (nums[mid] == target){
-                left = mid;
-            }
-            if (nums[mid] < target){
-                left = mid;
-            }            
-            if (nums[mid] > target){
-                right = mid;
-            }
-        }
-        
-        if (nums[left] == target){
-            arr[1] = left;
-        }
-        if (nums[right] == target){
-            arr[1] = right;
-        }
     }
+    
 }
