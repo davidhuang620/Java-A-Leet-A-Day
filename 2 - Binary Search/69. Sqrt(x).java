2413596b838276b0089090f03f437b7d0187ffe3
David Huang <d12yes@gmail.com>
2019-05-23 09:59:01 -0500

    Update 69. Sqrt(x).java

--- 2 - Binary Search/69. Sqrt(x).java
+++ 2 - Binary Search/69. Sqrt(x).java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public int mySqrt(int x) {
+        
+        // edge cases where x = 0, 1
+        if (x <= 1) return x;
+        
+        int low = 0, up = x;
+        while (low + 1 < up){
+            int pivot = low + (up - low)/2;
+            // prevent overflow, don't use pivot * pivot > x
+            if (pivot > x/pivot){
+                up = pivot;
+            } 
+            if (pivot <= x/pivot){
+                low = pivot;
+            }
+        }
+        return low;
+    }
+}
